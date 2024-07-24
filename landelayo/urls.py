491ba9a5e47dbc3d19b22530be391542b7ce7df1
knightebsuku <midnight.complex@protonmail.com>
2024-07-24 21:51:38 +0200

    custom view queryset requires basename

--- landelayo/urls.py
+++ landelayo/urls.py
@@ -3,6 +3,6 @@ from rest_framework import routers
 from landelayo.views import CalendarViewSet, EventViewSet, UpcomingViewSet
 
 router = routers.DefaultRouter()
-router.register(r'calendar', CalendarViewSet)
-router.register(r'event', EventViewSet)
+router.register(r'calendars', CalendarViewSet, basename='calendar')
+router.register(r'events', EventViewSet, basename='event')
 router.register(r'upcoming', UpcomingViewSet, basename='upcoming')
